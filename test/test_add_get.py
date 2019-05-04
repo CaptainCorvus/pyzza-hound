b227d65fcaa80aafe4001766a436d3bc80402ae9
Trevor <trevor.mccleer@gmail.com>
2019-05-04 16:27:36 -0700

    better naming

--- test/test_add_get.py
+++ test/test_add_get.py
@@ -1,4 +1,4 @@
-import SensorDB as sdb
+import SensorDB
 import datetime
 import bokeh.plotting as bp
 
@@ -6,14 +6,16 @@ TEST_ENTRY = {
     'comment': 'testadelphia'
 }
 
-s = sdb.SensorDBI()
+data_interface = SensorDB.SensorDBI()
 
 # s.add_to_testing(TEST_ENTRY)
 tstop = datetime.datetime.now()
-tstart = (tstop - datetime.timedelta(days=4))
-times, tempc, tempf = s.get_temp_readings(tstart, tstop, 'Pecan')
+tstart = (tstop - datetime.timedelta(hours=36))
+times, tempc, tempf = data_interface.get_temp_readings(tstart, tstop, 'Pecan')
 
 bp.output_file('test.html')
-p = bp.figure(title='Tempf vs Time', x_axis_type='datetime', x_axis_label='time', y_axis_label='Temp [F]')
-p.line(times, tempf, legend='Tempf/Tempc', line_width=2)
+p = bp.figure(title='Tempf vs Time', x_axis_type='datetime',
+              plot_height=500, plot_width=800,
+              x_axis_label='time', y_axis_label='Temp [F]')
+p.scatter(times, tempf, legend='Temperature', line_width=2)
 bp.show(p)
