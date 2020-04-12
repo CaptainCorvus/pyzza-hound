2a300bea9946ad906002d7311707fd68d7042847
Trevor <trevor.mccleer@gmail.com>
2020-04-12 13:16:03 -0700

    removed led code from rpi_temp, moved logic to function in led.py

--- pyzzaHound/leds.py
+++ pyzzaHound/leds.py
@@ -1,7 +1,20 @@
 import gpiozero
+import common
+
+logger = common.get_logger(__name__)
 
 LED = gpiozero.LED
 
 LEDS = (LED(26), LED(19), LED(13), LED(6),
         LED(12), LED(16), LED(20), LED(21))
 
+def display_temp_analog(temp):
+    logger.info("Updating LEDs")
+    # round temp, convert to int
+    temp = int(round(temp, 0))
+    for led in LEDS:
+        if temp & 1:
+            led.on()
+        else:
+            led.off()
+        temp = temp >> 1
